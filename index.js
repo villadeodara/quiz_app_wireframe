a4c28144884665a326c7284ee2cadae55f07df88
villadeodara <villadeodara@gmail.com>
2019-10-16 21:58:51 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const STORE = {
+  questions: [//1
+    {
+      question: "Designed by Catalan architect Antoni Gaudí, this Basilica is still unfinished after a century: ",
+      image_url: "https://images.pexels.com/photos/1062037/pexels-photo-1062037.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      options: [
+        "Basilica of Our Lady of Candelaria", 
+        "Sagrada Familia", 
+        "Basilica de la Macarena", 
+        "Basilica of Saints Justus and Pastor"
+      ],
+      answer: "Sagrada Familia"
+    },
+    //2
+    {
+      question: "This 900-year-old castle and fortress in central London is notable for housing the crown jewels:",
+      image_url: "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      options: [
+        "Windsor Castle",
+        "Buckingham Palace", 
+        "The Tower of London",
+        "Hampton Court Palace"
+      ],
+      answer: "The Tower of London"
+    },
+    //3
+    {
+      question: "This 18th-century sculpted fountain was featured in the movie 'Three coins in the fountain'",
+      image_url: "https://images.pexels.com/photos/2885919/pexels-photo-2885919.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      options: [
+        "Fontana del Tritone", 
+        "The Nasone", 
+        "Fontana della Barcaccia", 
+        "The Trevi Fountain"
+      ],
+      answer: "The Trevi Fountain"
+    },
+    //4
+    {
+      question: "This Opulent, 19th-century royal residence was built by King Ludwig II, and is the only one which he lived to see completed:",
+      image_url: "https://images.unsplash.com/photo-1565133060055-204fd1bf2e14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1648&q=80",
+      options: [
+        "Neuschwanstein Castle", 
+        "Hohenschwangau Castle", 
+        "Linderhof Palace", 
+        "Herrenchiemsee New Palace"],
+      answer: "Linderhof Palace"
+    },
+    //5
+    {
+      question: "In Paris, the Ladurée chain of pastry shops was established in 1862, and has been known for this french dessert: ",
+      image_url: "https://images.pexels.com/photos/205961/pexels-photo-205961.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      options: [
+        "Crème caramel", 
+        "Madeleine", 
+        "Mille-feuille", 
+        "Macaron"
+      ],
+      answer: "Macaron"
+    },
+    //6
+    {
+      question: "This museum in italy has a statue of Machiavelli near its entrance: ",
+      image_url: "https://cdn.pixabay.com/photo/2016/11/06/17/23/gallery-1803525_1280.jpg",
+      options: [
+        "Galleria Palatina", 
+        "Circuito Museale Boboli e Argenti", 
+        "Galleria degli Uffizi", 
+        "Gallerie dell’Accademia"
+      ],
+      answer: "Galleria degli Uffizi"
+    },
+    ],
+  currentQuestion: 0,
+  score: 0
+};
